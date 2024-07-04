4e227e604bbc6fedf3f1333b26e6648641decade
nfl6fh <89548640+nfl6fh@users.noreply.github.com>
2024-07-04 16:49:41 -0400

    calendar display and formatting

--- tracker/views.py
+++ tracker/views.py
@@ -19,24 +19,25 @@ def workout_calendar(request):
     week = []
     # get the first day of the month
     first_day = datetime.datetime(int(current_year), now.month, 1)
+    # print(first_day)
     # get the weekday of the first day of the month
     weekday = first_day.weekday()
     # get the number of days in the month
-    num_days = now.day
+    num_days = (datetime.datetime(int(current_year), (now.month+1), 1) - datetime.timedelta(days=1)).day
     # get the number of days in the previous month
     prev_month = now.month - 1
     prev_year = now.year
     if prev_month == 0:
         prev_month = 12
         prev_year = now.year - 1
-    num_days_prev_month = datetime.datetime(prev_year, prev_month, 1).day
+    num_days_prev_month = (first_day - datetime.timedelta(days=1)).day
+    print(num_days_prev_month)
     # get the number of days in the next month
     next_month = now.month + 1
     next_year = now.year
     if next_month == 13:
         next_month = 1
         next_year = now.year + 1
-    num_days_next_month = datetime.datetime(next_year, next_month, 1).day
     # fill in the days of the previous month
     for i in range(weekday):
         week.append(num_days_prev_month - weekday + i + 1)
@@ -47,7 +48,7 @@ def workout_calendar(request):
             weeks.append(week)
             week = []
     # fill in the days of the next month
-    for i in range(len(week), 7):
-        week.append(i - num_days + 1)
+    for i in range(1, 8-len(week)):
+        week.append(i)
     weeks.append(week)
     return render(request, 'tracker/workout_calendar.html', {'calendar': weeks, 'current_month': current_month, 'current_year': current_year})
